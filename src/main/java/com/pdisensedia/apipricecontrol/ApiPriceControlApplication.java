57d288144b4a079e10f731018d730f8022a12375
Your Name <you@example.com>
2023-08-10 14:30:15 -0300

    first commit

---
+++ src/main/java/com/pdisensedia/apipricecontrol/ApiPriceControlApplication.java
@@ -0,0 +1,13 @@
+package com.pdisensedia.apipricecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPriceControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPriceControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdisensedia/apipricecontrol/ApiPriceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdisensedia.apipricecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPriceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
